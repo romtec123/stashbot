041ad00012e5e7e75440e049c6ca95ea1faa3be7
romtec123 <julianlevin11@gmail.com>
2021-06-16 13:50:42 -0700

    Add in-progress smart reconnect system

--- index.js
+++ index.js
@@ -3,41 +3,87 @@ const Discord = require('discord.js');
 const dbot = new Discord.Client({ disableEveryone: true });
 const mineflayer = require('mineflayer');
 const request = require('request');
-const { chmodSync } = require("fs-extra");
+const fs = require("fs-extra");
 require('dotenv').config();
 
 console.log("\nStashbot starting...");
-
+mineflayerWait()
 
 //Mineflayer bot setup
 
-var options = {
-    host: process.env.IP,
-    port: process.env.PORT,
-    username: process.env.MCUSER,
-    password: process.env.MCPASS,
-    version: process.env.VERSION,
-    tokensLocation: './bot_tokens.json'
-};
-
-tokens.use(options, function(_err, _opts){
-    var bot = mineflayer.createBot(_opts);
-    bind(bot);
+async function writeStartTime() {
+    var jsonTime = {
+        "time": Date.now()
+    }
+    fs.writeFileSync(`./startTime.json`, JSON.stringify(jsonTime, null, 2));
+}
 
 
-    function bind(bot){
+function mineflayerWait() {
+    setTimeout(function(){ 
+        if(fs.existsSync(`./startTime.json`)){
+            var startTime = require(`./startTime.json`)
+            if((Date.now() - startTime.time) <= 30000){
+                var waitTime = (30000 - (Date.now() - startTime.time) + 5000) //Add 5 seconds because cocaine server
+                console.log(`Waiting ${waitTime}ms to start mineflayer to avoid connection throttling`)
+                setTimeout(function(){
+                    start()
+                }, waitTime);
+            } else{
+                start()
+            }
+            delete require.cache[require.resolve(`./startTime.json`)]
+        } else {
+            console.log("startTime.json missing. Creating file...")
+            start()
+        }
+    },500)
+}
 
-        console.log("\nMineflayer starting...");
-        bot.on('kicked', (reason, loggedIn) => console.log(`Bot was kicked: ${reason.text}`));
-        bot.on('error', err => console.error(err));
 
+function start() {
+
+
+    var options = {
+        host: process.env.IP,
+        port: process.env.PORT,
+        username: process.env.MCUSER,
+        password: process.env.MCPASS,
+        version: process.env.VERSION,
+        tokensLocation: './bot_tokens.json'
     };
 
+    tokens.use(options, function(_err, _opts){
+        var bot = mineflayer.createBot(_opts);
+        bind(bot);
 
-});
+
+        function bind(bot){
+
+            console.log("\nMineflayer starting...");
+            bot.on('login', function(){
+                writeStartTime()
+                console.log("Bot successfully logged in.")
+            });
+            bot.on('kicked', function(reason, loggedIn) {
+                var msg = JSON.parse(reason)
+                console.log(`Bot was kicked: ${msg.text}`)
+                writeStartTime()    
+            });
+            bot.on('error', err => console.error(err));
+            bot.on('end', function() {
+                console.log("Mineflayer bot disconnected. Restarting...")
+                setTimeout(function(){
+                    mineflayerWait()
+                },3000)
+            });
+
+        };
 
 
+    });
 
+}
 
 
 
