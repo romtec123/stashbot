c45763f34a4a7e6053d41c93632e92bd7cdfa396
cz <czho@protonmail.com>
2021-07-09 16:16:56 -0500

    proxy shit

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const dbot = new Discord.Client({ disableEveryone: true });
 const mineflayer = require('mineflayer');
 const request = require('request');
 const fs = require("fs-extra");
+const mcproxy = require("@rob9315/mcproxy");
+let mc = require("minecraft-protocol");
+
+let server, proxyClient;
 require('dotenv').config();
 
 console.log("\nStashbot starting...");
 mineflayerWait()
 
 //Mineflayer bot setup
+
+
+server = mc.createServer({ // create a server for us to connect to
+    'online-mode':  process.env.whitelist,
+    encryption: true,
+    host: "0.0.0.0",
+    port: process.env.PROXY_PORT,
+    version: process.env.VERSION,
+    'max-players': maxPlayers = 0
+});
 
 async function writeStartTime() {
     var jsonTime = {
@@ -52,10 +66,11 @@ function start() {
         version: process.env.VERSION,
         tokensLocation: './bot_tokens.json'
     };
+    
 
     tokens.use(options, function(_err, _opts){
-        var bot = mineflayer.createBot(_opts);
-        bind(bot);
+        var conn = new mcproxy.Conn(_opts);
+        bind(conn.bot);
 
 
         function bind(bot){
@@ -64,25 +79,44 @@ function start() {
             bot.on('login', function(){
                 writeStartTime()
                 console.log("Bot successfully logged in.")
+
+                server.maxPlayers = 1;
+
+                server.on('login', (newProxyClient) => {
+                    newProxyClient.on('packet', (data, meta, rawData) => { // redirect everything we do to 2b2t
+                        if (meta.name !== "keep_alive" && meta.name !== "update_time") { //keep alive packets are handled by the client we created, so if we were to forward them, the minecraft client would respond too and the server would kick us for responding twice.
+                            bot._client.writeRaw(rawData);
+                          }
+                    });
+                    newProxyClient.on('end', (data, meta, rawData) => {
+                        proxyClient = null;
+                    });
+                    conn.sendPackets(newProxyClient);
+                    conn.link(newProxyClient);
+                    //sets instance client to the current players client
+                    proxyClient = newProxyClient;
+                });
             });
             bot.on('kicked', function(reason, loggedIn) {
                 var msg = JSON.parse(reason)
                 console.log(`Bot was kicked: ${msg.text}`)
+                try {
+                    proxyClient.end("Bot kicked");
+                } catch {}
                 writeStartTime()    
             });
             bot.on('error', err => console.error(err));
             bot.on('end', function() {
                 console.log("Mineflayer bot disconnected. Restarting...")
+                try {
+                    proxyClient.end("Bot disconnected");
+                } catch {}
                 setTimeout(function(){
                     mineflayerWait()
                 },3000)
             });
-
         };
-
-
     });
-
 }
 
 
